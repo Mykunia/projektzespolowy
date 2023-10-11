d4d2f9747bcf6e9b382ba0bb62064b3d68d1c6d3
Marquess69 <marquess1224@gmail.com>
2023-10-11 12:29:07 +0200

    Models

---
+++ TeamProject/TeamProject/Shared/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject.Shared.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string PictureUrl { get; set; } = string.Empty;
+        public List<Picture> Pictures { get; set; } = new List<Picture>();
+        public Category? Category { get; set; }
+        public int CategoryId { get; set; }
+        public bool Featured { get; set; } = false;
+        public List<ProductVariant> Variants { get; set; } = new List<ProductVariant>();
+        public bool Visibility { get; set; } = true;
+        public bool Deleted { get; set; } = false;
+        [NotMapped]
+        public bool Edit { get; set; } = false;
+        [NotMapped]
+        public bool New { get; set; } = false;
+    }
+}
---
+++ TeamProject/TeamProject/Shared/User/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamProject.Shared.Models;
+
+namespace TeamProject.Shared.User
+{
+    public class User
+    {
+        //for bigger db is more secure to use guid rather than id
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public Address Address { get; set; }
+        //Standard role during register proccess
+        public string Role { get; set; } = "Customer";
+    }
+}
